8fa4928f710c4c3a1a615749c0cd824dcf11ca1d
regis <regismmat@gmail.com>
2023-08-07 19:43:26 -0300

    fim da aula

--- periodo1/dia1/aulaoffline/src/main.js
+++ periodo1/dia1/aulaoffline/src/main.js
@@ -1,8 +1,9 @@
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
+import './style.css';
 
 const senhaBtn = document.querySelector('button');
 const displaySenha = document.querySelector('h2');
 
 senhaBtn.addEventListener('click', () => {
-  displaySenha.innerHTML = nanoid()
-})
+  displaySenha.innerHTML = nanoid();
+});
